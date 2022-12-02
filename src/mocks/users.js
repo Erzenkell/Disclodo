4e34bb17986e252f69592fd66105100f2be3fe37
Erzenkell <ferrier.sammy@gmail.com>
2022-12-02 16:13:05 +0100

    m o c k s

--- src/mocks/users.js
+++ src/mocks/users.js
@@ -1,3 +1,5 @@
+const default_icon = "https://files.cults3d.com/uploaders/16080059/illustration-file/b5ee5801-0527-4e6f-9c16-95d0af5c61cd/Kirbo.png"
+
 export const usersMock = [
     {
         "name": "John Doe",
@@ -12,72 +14,72 @@ export const usersMock = [
     {
         "name": "John DoeDoeDoe",
         "id" : "3",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "Jean Mich",
         "id" : "5",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "Zoubi les potes",
         "id" : "6",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "Ta mere",
         "id" : "7",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "sisi la mif",
         "id" : "8",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "Amateur de pain",
         "id" : "9",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "youpi",
         "id" : "10",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "ouga bouga",
         "id" : "11",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "zib zab zoub",
         "id" : "12",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "alabatay",
         "id" : "13",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "AAAAAAAAAAAAA",
         "id" : "14",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "zzzzzzzzzzzz",
         "id" : "15",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "jemlep1",
         "id" : "16",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
     {
         "name": "rastapouek",
         "id" : "17",
-        "icon": 'https://i.imgur.com/4Y7YQXH.png',
+        "icon": default_icon,
     },
 ]
 
